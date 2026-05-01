2dad81935bbbc35640584ee59c229a8e3e7d4480
Kristinng24 <kristinng24@ru.is>
2026-05-01 11:21:37 +0000

    Functionality added to the addClass component, made changes to crunch.js

--- src/crunch.js
+++ src/crunch.js
@@ -1,37 +1,47 @@
+class CrunchCollection {
+    constructor(elements) {
+        this.elements = Array.from(elements);
+    }
+
+    addClass(className) {
+        this.elements.forEach((el) => el.classList.add(className));
+        return this;
+    }
+
+    removeClass(className) {
+        this.elements.forEach((el) => el.classList.remove(className));
+        return this;
+    }
+    
+    toggleClass(className) {
+        this.elements.forEach((el) => el.classList.toggle(className));
+        return this;
+    }
+
+    attr(name, value) {
+        if (value === undefined) {
+            return this.elements[0].getAttribute(name);
+        } else {
+            this.elements.forEach(el => el.setAttribute(name, value));
+            return this;
+        }
+    }
+
+    text(value) {
+        if (value === undefined) {
+            return this.elements[0].textContent;
+        } else {
+            this.elements.forEach(el => el.textContent = value);
+            return this;
+        }
+    }
+}
+
 function crunch(selector) {
     const elements = document.querySelectorAll(selector);
-    return{
-        elements: elements,
-        addClass(className) {
-            this.elements.forEach(el => el.classList.add(className));
-            return this;
-        },
-        removeClass(className) {
-            this.elements.forEach(el => el.classList.remove(className));
-            return this;
-        },
-        toggleClass(className) {
-            this.elements.forEach(el => el.classList.toggle(className));
-            return this;
-        },
-        attr(name, value) {
-            if (value === undefined) {
-                return this.elements[0].getAttribute(name);
-            } else {
-                this.elements.forEach(el => el.setAttribute(name, value));
-                return this;
-            }        
-        },
-        text(value) {
-            if (value === undefined) {
-                return this.elements[0].textContent;
-            } else {
-                this.elements.forEach(el => el.textContent = value);
-                return this;
-            }
-        }
-    };
+    return new CrunchCollection(elements);
 }
 
 window.crunch = crunch;
 
+export {crunch, CrunchCollection};
